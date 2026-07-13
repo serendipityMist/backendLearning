a35a457f0420fe9c207ddcfded4c12121fd0d953
serendipityMist <serendipitymist@gmail.com>
2026-07-13 07:17:18 +0545

    mongodb connection refused error solved

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import { setServers } from "node:dns";
+setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDB = async()=>{
     try {

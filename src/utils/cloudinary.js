9f4627d1a52df3eb4244d049dba10a1ad58b44ba
serendipityMist <serendipitymist@gmail.com>
2026-07-18 09:55:48 +0545

    Must supply API Key error solved

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,26 +1,38 @@
-import {v2 as cloudinary} from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+  secure: true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async(localfilepath)=>{
+
+const uploadOnCloudinary = async (localfilepath) => {
   try {
-    if(!localfilepath){
+    if (!localfilepath) {
       return null;
     }
-    const response = await cloudinary.uploader.upload(localfilepath,{resource_type:"auto"});
-    console.log(`File has been uploaded on cloudinary ${response}`);
-    return response;
+    const response = await cloudinary.uploader.upload(localfilepath, { resource_type: "auto" });
+    console.log(response);
     
-  } catch (error) {
+    // console.log(`File has been uploaded on cloudinary ${response}`);
     fs.unlinkSync(localfilepath);
+    return response;
+
+  } catch (error) {
+    // console.log("PORT:", process.env.PORT);
+    // console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+    // console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+    // console.log("Cloudinary Error:", error);
+      fs.unlinkSync(localfilepath);
+
     return null;
   }
 }
 
-export {uploadOnCloudinary};
+export { uploadOnCloudinary };
